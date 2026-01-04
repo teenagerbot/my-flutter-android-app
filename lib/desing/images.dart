4af879d64048265b1979999fdae1e5e85ac639cc
teenagerbot <voloda2004@i.ua>
2026-01-04 13:21:44 +0200

    added images

--- lib/desing/images.dart
+++ lib/desing/images.dart
@@ -2,3 +2,34 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 final SvgPicture MotorcycleImage = SvgPicture.asset('assets/images/Motorcycle'
     '.svg');
+final SvgPicture BusImage = SvgPicture.asset('assets/images/Bus'
+    '.svg');
+final SvgPicture CarImage = SvgPicture.asset('assets/images/Car'
+    '.svg');
+final SvgPicture TruckImage = SvgPicture.asset('assets/images/Truck'
+    '.svg');
+final SvgPicture TuktukImage = SvgPicture.asset('assets/images/Tuktuk'
+    '.svg');
+final SvgPicture BoardingImage = SvgPicture.asset('assets/images/Boarding'
+    '.svg');
+final SvgPicture DeliveryImage = SvgPicture.asset('assets/images/Delivery'
+    '.svg');
+final SvgPicture LoadImage = SvgPicture.asset('assets/images/Load'
+    '.svg');
+final SvgPicture ParkingImage = SvgPicture.asset('assets/images/Parking'
+    '.svg');
+final SvgPicture PickupImage = SvgPicture.asset('assets/images/Pickup'
+    '.svg');
+final SvgPicture RepairImage = SvgPicture.asset('assets/images/Repair'
+    '.svg');
+final SvgPicture UnboardingImage = SvgPicture.asset('assets/images/unboarding'
+    '.svg');
+final SvgPicture UnloadImage = SvgPicture.asset('assets/images/Unload'
+    '.svg');
+final SvgPicture CheckImage = SvgPicture.asset('assets/images/Check'
+    '.svg');
+final SvgPicture ArrowBackImage = SvgPicture.asset('assets/images/arrow_back'
+    '.svg');
+final SvgPicture AccountCircleImage = SvgPicture.asset
+  ('assets/images/account_circle'
+    '.svg');
