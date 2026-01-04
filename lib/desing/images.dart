2da690381f5309e1d07262b7094b1bcddaa5bec6
teenagerbot <voloda2004@i.ua>
2026-01-04 13:09:42 +0200

    added images

---
+++ lib/desing/images.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+final SvgPicture MotorcycleImage = SvgPicture.asset('assets/images/Motorcycle'
+    '.svg');
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,15 @@
 import "package:flutter/material.dart";
 
+import "desing/images.dart";
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Hello from home"),
+        child: MotorcycleImage,
       ),
     );
   }
