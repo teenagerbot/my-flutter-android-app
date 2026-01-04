d57e3d8b5c3c1584d5fa57bef328c320223c6e38
teenagerbot <voloda2004@i.ua>
2026-01-04 15:26:37 +0200

    Added screnns

--- lib/desing/dimensions.dart
+++ lib/desing/dimensions.dart
@@ -1,4 +1,6 @@
 /*Font sizes*/
+const double fontSize12 = 12;
+const double fontSize14 = 14;
 const double fontSize16 = 16;
 const double fontSize20 = 20;
 
@@ -9,6 +11,8 @@ const double height64 = 64;
 /*Paddings*/
 const double pad16 = 16;
 const double pad8 = 8;
+const double pad6 = 6;
+
 
 /*Elevations*/
 const double el006 = 0.06;
--- lib/pages/vehicle/vehicle_item.dart
+++ lib/pages/vehicle/vehicle_item.dart
@@ -12,27 +12,108 @@ class VehicleItem extends StatelessWidget {
     /*Row - строка карточки
     * InkWell - позволяет сделать клик или еще что то
     * */
-    return Card(
-      color: SurfaceColor,
-      elevation: el006,
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(bdr8)),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(bdr8),
-        onTap: () {},
-        child: Padding(
-          padding: const EdgeInsets.only(left: pad8, right: pad16),
-          child: Row(children: <Widget>[MotorcycleImage, _title(), _state()]),
+    return SizedBox(
+      height: height64,
+      child: Card(
+        color: SurfaceColor,
+        elevation: el006,
+        margin: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(bdr8),
+        ),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(bdr8),
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.only(left: pad8, right: pad16),
+            child: Row(children: <Widget>[MotorcycleImage, _title(), _state()]),
+          ),
         ),
       ),
     );
   }
 
   Widget _title() {
-    return Container();
+    /*Columns
+    * RichText - составной текст с разными стилями
+    * Expanded - растягивает блок по всей ширине
+    * */
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: pad6, right: pad6),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, //выравнивание текста
+          // справа
+          mainAxisAlignment: MainAxisAlignment.center, //по центру по вертикали
+          children: <Widget>[
+            const Text(
+              'BMW GS-7638',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: secondaryColor,
+                fontSize: fontSize14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            if (false)
+              Text(
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                'No driver',
+                style: TextStyle(
+                  color: secondaryVariantColor,
+                  fontSize: fontSize14,
+                  fontWeight: FontWeight.w400,
+                ),
+              )
+            else
+              RichText(
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                text: const TextSpan(
+                  style: TextStyle(fontSize: fontSize14),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "Driver: ",
+                      style: TextStyle(
+                        color: secondaryVariantColor,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Paul",
+                      style: TextStyle(
+                        color: secondaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _state() {
-    return Container();
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          PickupImage,
+          const Text(
+            "Pickup",
+            style: TextStyle(
+              color: secondaryVariantColor,
+              fontSize: fontSize12,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
