7cd6975934e4c130ad0a0ac83ebf2ce62f5ec88f
teenagerbot <voloda2004@i.ua>
2026-01-04 12:44:33 +0200

    Creating project
    ...what

---
+++ lib/desing/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFF8E1F);
+const Color secondaryColor = Color(0x081B31);
+const Color SecondaryVariant = Color(0x081B31);
+const Color backgroundColor = Color(0xF2F2F2);
+const Color SurfaceColor = Color(0xFFFFFF);
+//my colors
